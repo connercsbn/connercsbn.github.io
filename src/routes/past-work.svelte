be8d09278533d2553edc7dd2906a4d4caf00d258
Conner <65339198+connercsbn@users.noreply.github.com>
2022-03-11 02:31:59 -0800

    fix links

--- src/routes/past-work.svelte
+++ src/routes/past-work.svelte
@@ -9,14 +9,14 @@
 		<PortfolioIcon
 			name="The Cleveland Show Time Capsule Discord Bot"
 			href="https://top.gg/bot/938968621781225533"
-			icon="images/Cleveland_Brown.png"
+			icon="/images/Cleveland_Brown.png"
 			bgcolors={['#1D0917', '#380E21']}
 		/>
 
 		<PortfolioIcon
 			name="Albumsplit"
 			href="https://connercsbn.github.io"
-			icon="images/albumsplit_logo.png"
+			icon="/images/albumsplit_logo.png"
 			yoffset={-10}
 			bgcolors={['#94C881', '#83A79A']}
 			extrastuff="background-size: cover;"
@@ -25,7 +25,7 @@
 		<PortfolioIcon
 			name="Conner's Brian Website"
 			href="https://connersbrianwebsite.xyz"
-			icon="images/connersbrianwebsite.png"
+			icon="/images/connersbrianwebsite.png"
 			yoffset={-30}
 			bgcolors={['white', 'white']}
 			extrastuff="background-size: cover;"
@@ -34,7 +34,7 @@
 		<PortfolioIcon
 			name="Tax The Rich NYS"
 			href="https://www.taxtherichnys.com"
-			icon="images/tax_the_rich.png"
+			icon="/images/tax_the_rich.png"
 			bgcolors={['#FBF7E4', '#EB2329']}
 			xoffset={-8}
 			extrastuff="background-size: cover;"
@@ -43,7 +43,7 @@
 		<PortfolioIcon
 			name="Weirdle"
 			href="https://weirdle.web.app"
-			icon="images/weirdle.png"
+			icon="/images/weirdle.png"
 			bgcolors={['#B59F3B', '#538D4E']}
 			extrastuff="background-size: cover;"
 			xoffset={-5}
@@ -52,7 +52,7 @@
 		<PortfolioIcon
 			name="Deferred Capital Gain"
 			href="https://defer-225e4.web.app/"
-			icon="images/deferred_capital_gain.webp"
+			icon="/images/deferred_capital_gain.webp"
 			yoffset={60}
 			bgcolors={['white', 'white']}
 		/>
@@ -60,14 +60,14 @@
 		<PortfolioIcon
 			name="Google TTS Downloader"
 			href="https://www.conner.soy/pronunciation"
-			icon="images/tbd.png"
+			icon="/images/tbd.png"
 			bgcolors={['white', 'white']}
 		/>
 
 		<PortfolioIcon
 			name="Income Tax Calculator"
 			href="/"
-			icon="images/tax_the_rich.png"
+			icon="/images/tax_the_rich.png"
 			bgcolors={['white', 'white']}
 		/>
 	</main>
