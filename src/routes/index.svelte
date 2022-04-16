57ff81571c8fdf3ba5db7bc173534663bd9f8b9c
conner <connerluker@gmail.com>
2022-04-16 09:13:30 -0700

    add lorem ipsum

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
 
 <main>
 	<p>wbe developer and creator of WEIRDLE and connersbrianwebsite.xyz</p>
-	<p>featured on Destiny's stream</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit sed vulputate mi sit amet. Sem integer vitae justo eget magna fermentum iaculis. Egestas fringilla phasellus faucibus scelerisque. Morbi blandit cursus risus at ultrices mi. Ac turpis egestas sed tempus urna et. Eget gravida cum sociis natoque penatibus et. In hac habitasse platea dictumst quisque sagittis purus sit amet. Quisque non tellus orci ac auctor augue mauris augue. Ultricies leo integer malesuada nunc vel risus commodo viverra. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus. Mattis aliquam faucibus purus in massa tempor nec feugiat.</p>
 	<p>
 		svelte, react, django, next, celery, websockets, stripe, twilio, css/scss, linux, git, nginx,
 		firebase, mysql, postgresql, redis, threejs, nivo
